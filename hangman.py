4fd337b3d340c1d9802faaff1da9e7c3014eef23
Shivam Aery <shivamaeryy08@gmail.com>
2023-02-04 20:03:26 -0500

    Finish hangman game

--- hangman.py
+++ hangman.py
@@ -1,71 +1,9 @@
 import random
+import hangmanart
+from hangmanwords import word_list
 
-word_list = ["aardvark", "baboon", "camel"]
 
-stages = [
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========""",
-    """
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-""",
-]
+stages = hangmanart.stages
 
 
 def getMatchingCharacterIndices(word, letter):
@@ -81,26 +19,31 @@ end_of_game = False
 winner = False
 lives = 6
 word = random.choice(word_list)
+print(word)
 underscore_list = len(word) * ["_"]
+print(hangmanart.logo)
 print(stages[6])
 stage = 6
 guess = input("Enter a letter:").lower()
 while not end_of_game:
     if word.count(guess) > 0:
+        if guess in underscore_list:
+            print("You have already succesfully guessed:", guess)
         indices_of_letter = getMatchingCharacterIndices(word, guess)
         for indice in indices_of_letter:
             underscore_list[indice] = guess
-        print(underscore_list)
+        print(" ".join(underscore_list))
         if "".join(underscore_list) == word:
             end_of_game = True
             winner = True
         else:
-            guess = input("Enter a letter:")
+            guess = input("Guess a letter:")
 
     else:
         lives -= 1
         stage -= 1
         print(stages[stage])
+        print(f"The letter {guess} is not in the chosen word")
         print("Lives left:", lives)
         if lives == 0:
             break
